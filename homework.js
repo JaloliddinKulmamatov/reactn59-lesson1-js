6f8aab052b3700892545c315e938e5825fbe7d88
JaloliddinKulmamatov <itjalol2010@gmail.com>
2024-04-21 23:14:43 +0500

    jalol

---
+++ homework.js
@@ -0,0 +1,99 @@
+///1
+
+// function getFirstValue([a, b, c]) {
+//     return a
+// }
+// console.log(getFirstValue([80, 5, 100]));
+
+
+//2
+
+// const makePair = (a,b) =>   arr = [a,b];
+
+// console.log(makePair(512124, 215));
+
+//3
+
+// function removeItems(arr, num) {
+//     arr.splice(0, num);
+//     return arr
+// }
+
+// console.log(removeItems([1, 2, 3, 4, 5], 1));
+
+//4
+// const reverse  = (arr) =>  arr.reverse();
+
+//console.log(  reverse([9, 9, 2, 3, 4]));
+
+//5
+
+//5.1
+// function incrementItems(arr) {
+//     let newArr= [];
+//     for (let el of arr) {
+//         newArr.push(el + 1)
+//     }
+//     return newArr
+// }
+
+// console.log(  incrementItems([2, 4, 6, 8]) );
+
+//5.2
+
+//code
+
+//   function incrementItems(arr) {
+//     for (let i = 0; i < array.length; i++)
+//       arr[i] === arr[i] + 1
+//     return array
+//   }
+
+// //to'g'ri code
+
+//   function incrementItems(arr) {
+//     for (let i = 0; i < arr.length; i++){
+//       arr[i] = arr[i] + 1
+//   }
+//     return arr
+// }
+//  console.log(  incrementItems([2, 4, 6, 8]) );
+
+
+//6
+
+// const getLastItem = (arr) =>  arr.at(-1)
+
+// console.log(  getLastItem([true,false,true]) );
+
+//7
+
+// const arrayToString = (arr)  =>  arr.join("")
+
+// console.log(  arrayToString([1, 2, 3, "a", "s", "dAAAA"]));
+
+//8
+
+// function concat(arr1,arr2) {
+  // return arr1.concat(arr2)
+// }
+
+// console.log(  concat([1, 3, 5], [2, 6, 8]));
+
+//9
+
+// const findIndex = (arr, str) =>  arr.indexOf(str.toLowerCase());
+
+// console.log(    findIndex(["Red", "blue", "Blue", "Green"], "blue"));
+
+//10
+
+// function sumArray(arr) {
+//     let sum = 0
+//     for (const el of arr) {
+//         sum += el
+//     }
+//     return sum
+// }
+
+// console.log(  sumArray([0, 4, 8, 12]));
